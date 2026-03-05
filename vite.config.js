5c9cebbef09d51fac6678f30212ab947d9bf922c
kumarvivek93296-alt <kumarvivek93296@gmail.com>
2026-03-05 23:56:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,3 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+export default {
+  base: '/vivekarmy/', // Aapke repo naam ke saath
+  plugins: [react()],
+}
